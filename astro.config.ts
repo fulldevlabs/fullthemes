55bb12d0340f41f68f8441ffc6eb127fcd589db9
Dylan Kuipers <dylankuipers10@gmail.com>
2024-02-15 16:03:06 +0100

    styling fix..?

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,31 @@
+import sitemap from "@astrojs/sitemap";
+import { fulluiIntegration } from "@fulldevlabs/fullui/src/integration/fullui";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
-
-// ts file for fulluiintegration
-// doesnt work rn: TODO
+// @ts-ignore
+import bookshop from "@bookshop/astro-bookshop";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  site: import.meta.env?.PUBLIC_APP_URL || "https://localhost:4321",
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    bookshop(),
+    fulluiIntegration({
+      scheme: "light",
+      hue: {
+        base: "slate",
+        accent: "slate",
+      },
+    }),
+  ],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["astro:content"],
+      },
+    },
+  },
+});
