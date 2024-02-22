66600516574ae11adc1a7f3eb5d849608d5513bd
Dylan Kuipers <dylankuipers10@gmail.com>
2024-02-22 16:04:45 +0100

    content update, new fullui integration update testing, styling en meer

--- astro.config.ts
+++ astro.config.ts
@@ -2,34 +2,36 @@
 // TODO zie fullui-starter voor repo config, dus ook prettierconfig etc
 // TODO zie ook packagejson voor de packages
 
-import sitemap from "@astrojs/sitemap";
-import { fulluiIntegration } from "@fulldevlabs/fullui/src/integration/fullui";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap'
+import { fulluiIntegration } from '@fulldevlabs/fullui/src/integration/fulluiIntegration'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 // @ts-ignore
-import bookshop from "@bookshop/astro-bookshop";
+import bookshop from '@bookshop/astro-bookshop'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: import.meta.env?.PUBLIC_APP_URL || "https://localhost:4321",
+  output: 'static',
+  site: import.meta.env?.PUBLIC_APP_URL || 'https://localhost:4321',
   integrations: [
     sitemap(),
     robotsTxt(),
     bookshop(),
+    UnoCSS(),
     fulluiIntegration({
-      scheme: "light",
+      scheme: 'light',
       hue: {
-        base: "mauve",
-        accent: "jade",
+        base: 'mauve',
+        accent: 'jade',
       },
     }),
   ],
   vite: {
     build: {
       rollupOptions: {
-        external: ["astro:content"],
+        external: ['astro:content'],
       },
     },
   },
-});
+})
---
+++ src/content/pages/blog.md
@@ -0,0 +1,11 @@
+---
+_schema: default
+seo:
+  title: Lorem ipsum
+  description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+  image:
+    src:
+    alt:
+
+sections:
+---
